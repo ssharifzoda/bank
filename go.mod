e9c738cce967e9007c522da5ccff1109b00a540e
ssharifzoda <sharifzoda.sino@gmail.com>
2022-08-20 17:31:24 +0300

    init module and added types

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ssharifzoda/bank
+
+go 1.18
---
+++ pkg/types/types.go
@@ -0,0 +1,37 @@
+package types
+
+type Money int64
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+type Category string
+
+type PAN string
+
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+	MinBalance Money
+}
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+}
+
+type PaymentSource struct {
+	Type    string
+	Number  string
+	Balance Money
+}
